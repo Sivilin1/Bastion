2931457753f5e5e2d74521c275a176c7f735f0d7
Sivilin1 <89265738+Sivilin1@users.noreply.github.com>
2022-01-18 12:11:35 +0000

    Delete cypress-automation-testing directory

--- cypress-automation-testing/cypress/support/generate-random-username.ts
+++
@@ -1,5 +0,0 @@
-export function generateRandomUsername(): string {
-  return [...Array(6)]
-    .map(() => String.fromCharCode(97 + Math.floor(Math.random() * 26)))
-    .join('');
-}
--- cypress-automation-testing/cypress/types/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './pet-interface';
-export * from './order-interface';
